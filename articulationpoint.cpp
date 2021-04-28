003e20f430c5cc4d2b3582dbe24089f4dbeff359
Daniel Yuji Hosomi <d248255@dac.unicamp.br>
2021-04-27 22:15:14 -0300

    articulção

---
+++ articulationpoint.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define lsb(x) ((x)&(-x))
+#define R(x) ((x<<1)+1)
+#define L(x) (x<<1)
+#define ii pair<int,int>
+#define INF 1e9+1
+#define BUG(x) cout<<x<<endl;
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+const ll mod=1e9+7;
+//freopen("1.txt", "r", stdin);
+
+int N, M, low[10001], tin[10001], timer, ans;
+vector<int> V[10001], resp;
+
+void dfs(int A,int pai){
+    timer++;
+    tin[A]=low[A]=timer;
+    int qtd=0;
+    for(auto v : V[A]){
+        if(v==pai) continue;
+        if(tin[v])
+            low[A]=min(low[A],tin[v]);
+        else{
+            dfs(v,A);
+            qtd++;
+            low[A]=min(low[A],low[v]);
+            if(low[v]>=tin[A] && A!=pai)
+                ans++;
+        }
+    }
+    if(A==pai && qtd>1)
+        ans++;
+}
+
+int main(){_
+    
+    while(cin>>N>>M){
+    
+        while(M--){
+            int A, B; cin>>A>>B;
+            V[A].emplace_back(B);
+            V[B].emplace_back(A);
+        }
+
+        for(int i=1;i<=N;i++){
+            if(!tin[i])
+                dfs(i,i);
+        }
+
+        cout<<ans<<'\n';
+
+        for(int i=1;i<=N;i++){
+            V[i].clear();
+            tin[i]=low[i]=0;
+        }
+    }    
+    
+    return 0;
+}
