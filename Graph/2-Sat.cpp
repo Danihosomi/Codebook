41a31f6ea480902c2c2c2e3dc4b6f23209e66d53
Daniel Yuji Hosomi <danirtdhosomi@gmail.com>
2022-09-24 01:30:03 -0300

    corrijo o 2-Sat

--- Graph/2-Sat.cpp
+++ Graph/2-Sat.cpp
@@ -15,8 +15,8 @@ typedef pair<int,int>ii;
 typedef vector<int> vi;
 const ll mod = 1e9 + 7;
 
-int n, used[100005], vis[50005], comp[100005];
-vi v[100005], rv[100005], ord;
+int n, m, used[200005], vis[100005], comp[200005];
+vi v[200005], rv[200005], ord;
 
 void dfs1(int a){
     used[a]=1;
@@ -26,24 +26,35 @@ void dfs1(int a){
 
 void dfs2(int a,int c){
     comp[a]=c;
-    for(auto x : rv[a]) if(!comp[a]) dfs2(x,c);
+    for(auto x : rv[a]) if(!comp[x]) dfs2(x,c);
 }
 
 int ans2Sat(){
     ord.clear();
-    for(int i=1;i<=n;i++) if(!used[i]) dfs1(i);
-
-    for(int i=0,j=1;i<n;i++){
-        int x=ord[n-i-1];
-        if(!comp[x]) dfs2(x,j), j++;
+    for(int i=1;i<=2*n;i++) if(!used[i]) dfs1(i);
+    
+    for(int i=0,j=1;i<2*n;i++){
+        int x=ord[2*n-i-1];
+        if(!comp[x]) dfs2(x,j++);
     }
 
-    for(int i=1;i<=n;i+=2){
-        if(comp[i]==comp[i+1]) return 0;
-        vis[i/2]=(comp[i]>comp[i+1]);
+    for(int i=1;i<=n;i++){
+        if(comp[i]==comp[i+n]) return 0;
+        vis[i]=(comp[i]>comp[i+n]);
     }
 
     return 1;
+}
+
+void addAresta(bool posa,int a,bool posb,int b){
+    int nega=(posa ? a+n : a);
+    int negb=(posb ? b+n : b);
+    a=(nega>n ? a : a+n); b=(negb>n ? b : b+n);
+
+    v[nega].pb(b);
+    v[negb].pb(a);
+    rv[b].pb(nega);
+    rv[a].pb(negb);
 }
 
 int main(){_
