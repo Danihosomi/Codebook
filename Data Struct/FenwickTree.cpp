e6cc7814a476c9cfa9222bd2b4c1c79553e8d0ca
Fernando Morato <fernando.morato@incognia.com>
2023-06-20 18:42:44 -0300

    Refactor Fenwick Tree

--- Data Struct/FenwickTree.cpp
+++ Data Struct/FenwickTree.cpp
@@ -1,20 +1,39 @@
-#define lsb(x) ((x)&(-x))
-namespace FW{
-    int fw[200005], n;
+// Fenwick Tree
+//
+// Usage: FenwickTree<int> bit;
+//
+// 29985a
+template <typename T>
+class FenwickTree {
+public:
+	void init(int _n) {
+		n = _n;
+		bit.assign(n + 1, 0);
+	}
 
-    void update(int x,int val){
-        while(x<=n){
-            fw[x]+=val;
-            x+=lsb(x);
-        }
-    }
+	void init(vector<T> &v) {
+		n = (int) v.size();
+		bit.assign(n + 1, 0);
+		for (int i = 1; i <= n; i++) {
+			bit[i] += v[i - 1];
+			if (i + (i & -i) <= n)
+				bit[i + (i & -i)] += bit[i];
+		}
+	}
 
-    int query(int x){
-        int ans=0;
-        while(x){
-            ans+=fw[x];
-            x-=lsb(x);
-        }
-        return ans;
-    }
+	void update(int i, T x) {
+		for (; i <= n; i += i & -i)
+			bit[i] += x;
+	}
+
+	T query(int i) {
+		T ans = 0;
+		for (; i > 0; i -= i & -i)
+			ans += bit[i];
+		return ans;
+	}
+
+private:
+	int n;
+	std::vector<T> bit;
 };
