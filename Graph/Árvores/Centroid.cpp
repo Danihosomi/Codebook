15ab53c1cc139a10cf6b407c680a0eebb79c52e8
Danihosomi <danirtdhosomi@gmail.com>
2023-07-22 17:00:33 -0300

    refactoring code

--- Graph/Árvores/Centroid.cpp
+++ Graph/Árvores/Centroid.cpp
@@ -30,15 +30,27 @@ ii parCentroid(){
 	return {ans1,ans2};
 }
 
+void solve(int a,int p,int fi,int dist=1){
+	if(dist>k) return;
+	maior = max(dist,maior);
+	
+	if(fi) cnt[dist]++;
+	else ans+=cnt[k-dist];
+	
+	for(auto x : v[a])
+		if(!vis[x] && x!=p) solve(x,a,fi,dist+1);
+}
+ 
 void build(int a,int p=-1){ // Faz a Decomposition do Centroid
-    findSize(a,a);
-
-    int c = findCentroid(a,a,tam[a]);
-    vis[c]=1;
-    if(p!=-1) pai[c]=p;
-    
-
+    int c = findCentroid(a,a,findSize(a,a)); vis[c]=1; maior = 0;
+ 
+	for(auto x : v[c])
+		if(!vis[x]){
+			solve(x,c,0); // pego resposta 
+			solve(x,c,1); // dou update
+		}
+ 
+	fill(cnt+1,cnt+maior+1,0);
     for(auto x : v[c])
         if(!vis[x]) build(x,c);
 }
-
